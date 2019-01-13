65f0a843376219f5a0a6b73a96c1df299e0e8a66
keglatz <keglatz@users.noreply.github.com>
2019-01-13 16:44:44 +0100

    Added Rose Section and made fragment -> activity Intent

--- ProjectPrototype/app/src/main/java/com/example/kevin/projectprototype/activity/HomeFragment.java
+++ ProjectPrototype/app/src/main/java/com/example/kevin/projectprototype/activity/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.kevin.projectprototype.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.kevin.projectprototype.R;
 
@@ -15,6 +18,38 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+        ImageView whiteWine = (ImageView) v.findViewById(R.id.ivWhiteWine);
+        whiteWine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), WhiteWineFlavour.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView redWine = (ImageView) v.findViewById(R.id.ivRedWine);
+        redWine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RedWineFlavour.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView roseWine = (ImageView) v.findViewById(R.id.ivRoseWine);
+        roseWine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RoseWineFlavour.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
+
     }
 }
---
+++ ProjectPrototype/app/src/main/java/com/example/kevin/projectprototype/activity/RoseWineFlavour.java
@@ -0,0 +1,15 @@
+package com.example.kevin.projectprototype.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.kevin.projectprototype.R;
+
+public class RoseWineFlavour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_rose_wine_flavour);
+    }
+}
